59e72e500ebd845c385401ba2cd045156c9997d8
shonazarovmuzrob34-alt <shonazarovmuzrob34@gmail.com>
2026-01-07 15:07:09 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from config import BOT_TOKEN
+from handlers.start import start_router
+from handlers.language import language_router
+from handlers.menu import menu_router
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    dp.include_router(start_router)
+    dp.include_router(language_router)
+    dp.include_router(menu_router)
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
